d2b6d4b19b04626ea9b9bf2781a02a3792caebe8
Anderson Motta <anderson.motta@tegra.com.br>
2023-07-18 10:54:41 -0300

    remove commit

--- agenda.js
+++ agenda.js
@@ -1,5 +1,3 @@
-// https://www.youtube.com/watch?v=0izA25HmkaE
-
 const cabecalho = document.querySelector("#cabecalho");
 const menu = document.querySelector("#menu");
 const btn_home = document.querySelector("#btn_home");
